94019577c9cb823ff7904b256aac1c2df7c22cc4
mbero <mberendt546@gmail.com>
2020-04-30 20:36:41 +0200

    Added missing import to app.module.ts
    Angular material reinstalled

--- budget-app-frontend/application/src/app/app.module.ts
+++ budget-app-frontend/application/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localePl from '@angular/common/locales/pl';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NewExpenseComponent } from './components/new-expense/new-expense/new-expense.component';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MAT_CHIPS_DEFAULT_OPTIONS, MatChipsModule } from '@angular/material/chips';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,13 +29,23 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     NewExpenseComponent
   ],
   imports: [
+    MatFormFieldModule,
     BrowserModule,
-    ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    HttpClientModule,
     MatCardModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatTableModule,
     MatChipsModule,
-    MatAutocompleteModule
+    MatIconModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [{ provide: MAT_CHIPS_DEFAULT_OPTIONS, useValue: { separatorKeyCodes: [13, 188 ]}}],
   bootstrap: [AppComponent]
