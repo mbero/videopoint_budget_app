a019291180c5c8f6ad95291274dc5502e1c3a712
mbero <mberendt546@gmail.com>
2020-05-11 20:46:44 +0200

    Continuation of work with edit-expense-modal

--- budget-app-frontend/application/src/app/components/expenses-table/edit-expense-modal/edit-expense-modal/edit-expense-modal.component.ts
+++ budget-app-frontend/application/src/app/components/expenses-table/edit-expense-modal/edit-expense-modal/edit-expense-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Expense } from 'src/app/models/Expense';
 import { ExpenseEditOperation } from 'src/app/models/ExpenseEditOperation';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-expense-modal',
@@ -11,19 +12,30 @@ import { ExpenseEditOperation } from 'src/app/models/ExpenseEditOperation';
 export class EditExpenseModalComponent implements OnInit {
 
   public operationType: String;
+  public editForm: FormGroup;
+
+  public get dateCtrl(): FormControl{
+    return this.editForm.get('date') as FormControl;
+  }
 
   constructor(
     public dialogRef : MatDialogRef<EditExpenseModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ExpenseEditOperation
   ) { 
       this.operationType = data.operationType;
+
+      this.editForm = new FormGroup({
+        date: new FormControl(data.expense.date, Validators.compose([Validators.required])),
+        value: new FormControl(data.expense.value, Validators.compose([Validators.required, Validators.pattern('^(?:0|[1-9][0-9]*)\.[0-9]+$')])),
+        tags: new FormControl(data.expense.tags.map(el=> el.name), Validators.compose([Validators.required]))
+      });
   }
 
   ngOnInit(): void {
 
   }
 
-  public okCLick(){
+  public cancelClickHandler(){
     this.dialogRef.close();
   }
 
