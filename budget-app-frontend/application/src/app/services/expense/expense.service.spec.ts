ce7dd02399f55c2915cb9aa5eab01c921c4ecfe1
mbero <mberendt546@gmail.com>
2020-06-13 13:52:07 +0200

    Start of work related to implementation UI tests

--- budget-app-frontend/application/src/app/services/expense/expense.service.spec.ts
+++ budget-app-frontend/application/src/app/services/expense/expense.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ExpenseService } from './expense.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('ExpenseService', () => {
   let service: ExpenseService;
+  let httpTestingController: HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        ExpenseService
+      ]
+    });
     service = TestBed.inject(ExpenseService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy();    
   });
+
+
+
 });
