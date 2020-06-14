98bddc104858da00696f07df1ad62fabae275b17
mbero <mberendt546@gmail.com>
2020-06-14 14:12:36 +0200

    Added first test cases for new-expense.component

--- budget-app-frontend/application/src/app/app-routing.module.ts
+++ budget-app-frontend/application/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NewExpenseComponent } from './components/new-expense/new-expense/new-ex
 import { ExpensesTableComponent } from './components/expenses-table/expenses-table/expenses-table.component';
 
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: NewExpenseComponent },
   { path: 'expenses-table', component: ExpensesTableComponent },
   { path: 'new-expense', component: NewExpenseComponent }
--- budget-app-frontend/application/src/app/components/new-expense/new-expense/new-expense.component.spec.ts
+++ budget-app-frontend/application/src/app/components/new-expense/new-expense/new-expense.component.spec.ts
@@ -1,38 +1,78 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync, inject } from '@angular/core/testing';
 
+import { Location } from "@angular/common";
 import { NewExpenseComponent } from './new-expense.component';
-import { ExpenseService } from 'src/app/services/expense/expense.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TagService } from 'src/app/services/tag/tag.service';
-import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ExpensesTableComponent } from '../../expenses-table/expenses-table/expenses-table.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, RouterModule } from '@angular/router';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { ExpenseService } from 'src/app/services/expense/expense.service';
+import { routes, AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('NewExpenseComponent', () => {
   let component: NewExpenseComponent;
   let fixture: ComponentFixture<NewExpenseComponent>;
+  let location: Location;
+  let router: Router;
   let httpTestingController: HttpTestingController;
-  let expenseService: ExpenseService;
+  let expensesService: ExpenseService;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewExpenseComponent ],
-      imports: [HttpClientTestingModule, MatDialogModule, MatAutocompleteModule],
-      providers: [ExpenseService]
+      declarations: [NewExpenseComponent],
+      imports: [HttpClientModule, HttpClientTestingModule, MatDialogModule, MatAutocompleteModule,
+         RouterTestingModule.withRoutes(routes)],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        ExpenseService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+  }));
 
-    expenseService = TestBed.inject(ExpenseService);
-    httpTestingController = TestBed.inject(HttpTestingController);
+  beforeEach(async(() => {
+    expensesService = TestBed.get(ExpenseService);
+    router = TestBed.get(Router);
+    fixture = TestBed.createComponent(NewExpenseComponent);
+    location = TestBed.get(Location);
+    component = fixture.componentInstance;
+    router.initialNavigation();
+    httpTestingController = TestBed.get(HttpTestingController);
 
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NewExpenseComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should redirect to expenses-table component', fakeAsync(() => {
+    let expensesTablePath = "/expenses-table";
+    let button = fixture.debugElement.nativeElement.querySelector("#showExpensesButton");
+    fixture.detectChanges();
+    if (button) {
+      button.click();
+      //We are waiting for all pending promises to be resolved
+      tick();
+
+      fixture.whenStable().then(() => {
+        console.log('retreive location somehow');
+        let path = location.path();
+        expect(expensesTablePath).toBe(path);
+      });
+    }
+  }));
+
+
+
+
 });
