97322f3a85564f87136bf40fb80e95d554466652
mbero <mberendt546@gmail.com>
2020-06-14 13:36:46 +0200

    Finishing few test-cases for expense service

--- budget-app-frontend/application/src/app/services/expense/expense.service.spec.ts
+++ budget-app-frontend/application/src/app/services/expense/expense.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ExpenseService } from './expense.service';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ExpenseService', () => {
   let service: ExpenseService;
@@ -19,11 +19,11 @@ describe('ExpenseService', () => {
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();    
+    expect(service).toBeTruthy();
   });
 
-  it('should return proper Expense based on params', () =>{
-    let tags = [ { name: 'someNewTagName' }];
+  it('should return proper Expense based on params', () => {
+    let tags = [{ name: 'someNewTagName' }];
     let value = 19.90;
 
     let expectedResponseFromData = {
@@ -33,7 +33,78 @@ describe('ExpenseService', () => {
 
     let expenseFromData = service.getExpenseFromData(tags, value);
     expect(expectedResponseFromData.value).toBe(expenseFromData.value);
-  });  
+  });
 
+  it('should return all expenses in proper format', () => {
+
+    const expensesResponse =
+    {
+      "expenses": [
+        {
+          "id": 28,
+          "creationDate": "2020-06-16T22:00:00",
+          "value": 20112,
+          "tags": [
+            {
+              "id": 1,
+              "name": "tag1"
+            },
+            {
+              "id": 29,
+              "name": "weleflwlfewrlwer"
+            }
+          ],
+          "formattedDate": "2020-06-16T22:00:00.000Z"
+        }
+      ]
+    };
+
+    service.getAllExpenses().subscribe(response=>{
+        expect(response.length).toEqual(1);
+    });
+
+    const request = httpTestingController.expectOne('http://localhost:8083/expenses');
+
+    expect(request.request.method).toBe('GET');
+
+    request.flush(expensesResponse);
+  });
+
+  it('should return proper expense object as response', () => {
+
+    const expectedResponseBody = {
+      "expenses": [
+        {
+          "id": 32,
+          "creationDate": "2020-06-05T08:09:36.9607723",
+          "value": 2020,
+          "tags": [
+            {
+              "id": 31,
+              "name": "someNewTagName"
+            }
+          ],
+          "formattedDate": null
+        }
+      ]
+    };
+
+    let tag = { name: "someNewTagName"};
+    let exampleTags = [tag];
+
+    let testExpenseBody = service.getExpenseFromData(exampleTags, 2020);
+
+    service.addExpense(testExpenseBody).subscribe(response=>{
+      expect(response["expenses"].length).toEqual(1);
+    });
+
+    const request = httpTestingController.expectOne('http://localhost:8083/expense');
+    
+    expect(request.request.method).toBe('POST');
+
+    request.flush(expectedResponseBody);
+
+
+  });
 
 });
